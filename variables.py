1ace4f2ff5c7a9a2b136939748093fe924486c3c
shayankhalid-cmd <shayan.khalid@renacademy.org>
2025-09-08 17:35:58 -0600

    variables

---
+++ variables.py
@@ -0,0 +1,11 @@
+print("learning about variables")
+x=9
+y="variable"
+print ("the value of x =",x)
+print (y)
+name=input ("enter your name")
+print ("name",name)
+age=input("enter your age")
+print("age",age)
+hobby=input ("enter your hobby")
+print ("hobby",hobby)
