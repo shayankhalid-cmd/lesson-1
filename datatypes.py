4fd8394faf4ea4d4cb9e7e3ff0eae0e853b34395
shayankhalid-cmd <shayan.khalid@renacademy.org>
2025-09-09 17:27:48 -0600

    data types

---
+++ datatypes.py
@@ -0,0 +1,8 @@
+a=8
+print ("the data type of a is",type(a))
+b=9.7 
+print ("the data type of b is",type (b))
+c="shayan"
+print ("the data type of c is",type (c))
+d=True
+print ("the data type of d is",type(d))
