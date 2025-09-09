55a08b27f27f7706ae154a6b3591819a0ea85921
shayankhalid-cmd <shayan.khalid@renacademy.org>
2025-09-09 17:50:38 -0600

    casting

---
+++ casting.py
@@ -0,0 +1,18 @@
+name = "shayan"
+age = 10
+is_student = True
+weight = 38.7
+print ("name =", name)
+print ("the type of name is",type (name))
+print ("age =",age)
+print ("the type of age is",type (age))
+print ("is_student =", is_student)
+print ("the type of is_student",type(is_student))
+print ("weight=",weight)
+print ("the type of weight is",type(weight))
+print ("after typecasting")
+age = str(age)
+print ("the type of age is",type(age))
+weight = int(weight)
+print ("weight =",weight)
+print ("the type of weight is",type(weight))
