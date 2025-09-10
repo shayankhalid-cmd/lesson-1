6004239e043116a9a53e10dba8296c6ca027155b
shayankhalid-cmd <shayan.khalid@renacademy.org>
2025-09-10 17:34:54 -0600

    avrage.py

---
+++ avrage.py
@@ -0,0 +1,9 @@
+tree1=int(input("enter the hight of tree1 here:"))
+tree2=int(input("enter the hight of tree2 here:"))
+tree3=int(input("enter the hight of tree3 here:"))
+tree4=int(input("enter the hight of tree4 here:"))
+tree5=int(input("enter the hight of tree5 here:"))
+sum=tree1+tree2+tree3+tree4+tree5
+print ("the sum of all trees is :",sum)
+avrage=sum/5
+print("the avrage of the trees is:",avrage)
