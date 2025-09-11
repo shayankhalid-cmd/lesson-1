3264108f8ba53209283f05769a5dfdc3aadf9369
shayankhalid-cmd <shayan.khalid@renacademy.org>
2025-09-11 17:35:32 -0600

    greater or smaller

---
+++ greater or smaller.py
@@ -0,0 +1,8 @@
+h = int(input("enter price of product here:"))
+if h>15:
+    print ("h is greater than 15")
+    print ("i'm in if block")
+else:
+    print (" h is smaller than 15")
+    print ("i'm in else block")
+print ("i'm not in if or else block,i'm outside")
