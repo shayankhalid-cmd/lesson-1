3900c895dd0936100e7480bb159d92a2d1ac270a
shayankhalid-cmd <shayan.khalid@renacademy.org>
2025-09-10 20:16:58 -0600

    homework

---
+++ homework.py
@@ -0,0 +1,3 @@
+print ("the remainder amount after removing 100's:","amount%100")
+n =int (input("enter the number to find square root"))
+print ("the square root of n :", n**0.5)
---
+++ practice.py
@@ -0,0 +1,38 @@
+'''print("Hello world")
+a=5
+print(a)
+print("the valu of a is :",a)
+print(type(a))
+print("the type of a is:",type(a))
+b="Hello Python"
+print("The valu of b is :",b)
+print("The length of String is:",len(b))
+print("the string in upper case:",b.upper())
+print("The string in lower case:",b.lower())
+print("The string from 3to 5 is :",b[3:5])
+print("The string from 3to 5 is :",b[0:len(b)])
+print("The string from 3to 5 is :",b[0:len(b):2])
+
+
+tree1=input("Enter the height of tree1:")
+tree2=input("Enter the height of tree2:")
+sum=tree1+tree2
+print("The sum is:",sum)
+
+
+tree1=int(input("Enter the height of tree1:"))
+tree2=int(input("Enter the height of tree2:"))
+sum=tree1+tree2
+print("The sum is:",sum)'''
+
+
+#t1=int(input("Enter t1 :"))
+
+amount=int(input("Enter amount:"))
+print("The 100's present in my amount:",amount/100)
+amount=int(input("Enter amount:"))
+print("The 100's present in my amount:",amount/100)
+amount=int(input("Enter amount:"))
+print("The 100's present in my amount:",amount//100)
+
+print("The remainder amount after removing 100's:",amount%100)
